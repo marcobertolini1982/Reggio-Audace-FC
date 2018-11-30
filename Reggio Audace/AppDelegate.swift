4a0b2abe8266712eb1af4dc4a0f54d2803fcf81b
Marco Bertolini <marco.bertolini@lensolution.it>
2018-11-30 12:35:05 +0100

    Firebase Installed

--- Reggio Audace/AppDelegate.swift
+++ Reggio Audace/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -16,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
        
 
         // Override point for customization after application launch.
