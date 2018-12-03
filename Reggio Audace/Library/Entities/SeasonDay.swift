269c2f32beab7c1d34382525ca8c65e9ad13b74f
Michele <marco.bertolini@lemsolution.it>
2018-12-03 11:43:01 +0100

    sddaassssddddd

---
+++ Reggio Audace/Library/Entities/SeasonDay.swift
@@ -0,0 +1,33 @@
+//
+//  SeasonDay.swift
+//  Reggio Audace
+//
+//  Created by Michele on 03/12/18.
+//  Copyright © 2018 LEN Solution. All rights reserved.
+//
+
+import Foundation
+
+class SeasonDay
+{
+   var prg_lastuser:    Int?
+   var dat_lastmodified:String?
+   var prg_seasonday:   Int64?
+   var prg_season:      Int64?
+   var num_seasonday:   Int64?
+   var des_seasonday:   Int64?
+   var des_freefield1:  String?
+   var des_freefield2:  String?
+   var des_freefield3:  String?
+   var des_freefield4:  String?
+   var num_freefield1: Int64?
+   var num_freefield2: Int64?
+   var num_freefield3: Int64?
+   var num_freefield4: Int64?
+   var num_freefield5: Int64?
+   var num_freefield6: Int64?
+   var dat_freefield1: String?
+   var dat_freefield2: String?
+   var flg_freefield1: Bool?
+   var flg_freefield2: Bool?
+}
---
+++ Reggio Audace/Utils/StoryboardUtils.swift
@@ -0,0 +1,10 @@
+//
+//  StoryboardUtils.swift
+//  Reggio Audace FC App
+//
+//  Created by Michele on 23/11/18.
+//  Copyright © 2018 LEN Solution. All rights reserved.
+//
+
+import UIKit
+let MainStoryboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
