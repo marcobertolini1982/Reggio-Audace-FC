40cc4ec525b4c5606e1e9afbcbe122af39722404
Michele <marco.bertolini@lemsolution.it>
2019-01-18 18:04:58 +0100

    ssss

--- Reggio Audace/Views/CollectionViews/CviHeader.swift
+++ Reggio Audace/Views/CollectionViews/CviHeader.swift
@@ -10,20 +10,32 @@ import UIKit
 
 class CviHeader: UICollectionView
 {
-    private var SELECTEDEVENT:((IndexPath?)->Void)?
-    private var DESELECTEDEVENT:((IndexPath)->Void)?
-
+    private var SELECTEDEVENT:((Int)->Void)?
+    private var DESELECTEDEVENT:((Int)->Void)?
+    
+    public final var SelectedEvent:((Int)->Void)?
+    {
+        get{return self.SELECTEDEVENT}
+        set{self.SELECTEDEVENT = newValue}
+    }
+    
+    public final var DeselectedEvent:((Int)->Void)?
+    {
+        get{return self.DESELECTEDEVENT}
+        set{self.DESELECTEDEVENT = newValue}
+    }
     override func selectItem(at indexPath: IndexPath?, animated: Bool, scrollPosition: UICollectionView.ScrollPosition)
     {
         super.selectItem(at: indexPath, animated: animated, scrollPosition: scrollPosition)
-        self.SELECTEDEVENT?(indexPath)
+        guard indexPath != nil else{return}
+        self.SELECTEDEVENT?(indexPath!.item)
       
     }
     
     override func deselectItem(at indexPath: IndexPath, animated: Bool)
     {
         super.deselectItem(at: indexPath, animated: animated)
-        self.DESELECTEDEVENT?(indexPath)
+        self.DESELECTEDEVENT?(indexPath.item)
     }
 
 }
