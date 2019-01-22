76bc70f8048b6f6e965bd3fc90a35b1c41fa3d90
Michele <marco.bertolini@lemsolution.it>
2019-01-22 18:11:14 +0100

    wsswwaa

--- Reggio Audace/ViewControllers/Controllers/CtlPost.swift
+++ Reggio Audace/ViewControllers/Controllers/CtlPost.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs
+class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs,UITableViewDelegate,UITableViewDataSource
 {
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+       return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+      return self.POSTPOLLS.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let l_Cell:UITableViewCell = UITableViewCell()
+        l_Cell.backgroundColor = GARNETCOLOR
+        return l_Cell
+    }
+    
+   
+    
     // Properties
     private  var POST:Post?
     private var POSTPOLLS:[PostPoll] = [PostPoll]()
@@ -46,8 +64,22 @@ class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs
     {
        
         super.viewDidLoad()
-        self.VIEPOST.frame = self.view.bounds
+        //self.VIEPOST.frame = self.view.bounds
+       
+        //let l_Tableview:UITableView = UITableView(frame: CGRect(x:200.0,y:200.0,width:200.0,height:200.0))
+        //l_Tableview.backgroundColor = ColorUtils.red
+       // l_Tableview.delegate = self
+        //l_Tableview.dataSource = self
+        //self.view.sendSubviewToBack(l_Tableview)
+        //elf.view.addSubview(l_Tableview)
         self.view.addSubview(self.VIEPOST)
+        self.VIEPOST.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0).isActive = true
+        self.VIEPOST.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0.0).isActive = true
+        self.VIEPOST.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0).isActive = true
+        self.VIEPOST.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant:0.0).isActive = true
+        
+       
+       
         
     }
     
@@ -60,6 +92,7 @@ class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs
             self.VIEPOST.txt_Article.text = self.POST?.des_post
             self.VIEPOST.lbl_Comment.text = "\(self.POST?.num_postmessages ?? Int64())"
             self.VIEPOST.lbl_Reactions.text = "\(self.POST?.num_reactions ?? Int64())"
+            
         }
         
     }
@@ -96,7 +129,8 @@ class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs
          guard let l_Parent:PagPost = self.parent as? PagPost else{return}
          self.LoadPoastContent(prg_post: l_Parent.PrgPost)
          self.LoadPostPolls()
-         print(POSTPOLLS.count)
+       
+        
     }
     
     private final func LoadPostPolls()
@@ -105,4 +139,7 @@ class CtlPost: CtlBase,ProSinglePostObs,ProFileObs,ProPostPollObs
         l_PostPollView.SetPostPollsLoaded(propostpollsobs: self)
         l_PostPollView.LoadPostPolls(prg_post: self.Parent?.PrgPost)
     }
+    
+    
+    
 }
