40919b41d94f07ebc2aa42b53b0f9a594f0c9fea
Michele <marco.bertolini@lemsolution.it>
2019-01-24 13:39:46 +0100

    Modificata la classe delradiobutton

--- Reggio Audace/Buttons/BtnRadio.swift
+++ Reggio Audace/Buttons/BtnRadio.swift
@@ -13,10 +13,9 @@ class BtnRadio: BtnBase
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
-        self.layer.borderWidth = 3.0
+        self.layer.borderWidth = 2.0
         self.layer.borderColor = GARNETCOLOR.cgColor
-        self.layer.cornerRadius = self.bounds.width/2
-        self.clipsToBounds = true
+        self.layer.cornerRadius = self.bounds.width/2.0
         self.contentMode = ContentMode.scaleAspectFit
         //self.contentEdgeInsets = UIEdgeInsets(top: 7.5, left: 7.5, bottom: 7.5, right: 7.5)
     }
