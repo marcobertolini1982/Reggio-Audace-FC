74db183b0cf4e61670db6f65af03622781054142
Michele <marco.bertolini@lemsolution.it>
2019-01-25 12:02:57 +0100

    Aggiunte le classi  per i bottoni con forma rotonda

--- Reggio Audace/Buttons/RoundShapedButtons/BtnCommentSender.swift
+++ Reggio Audace/Buttons/RoundShapedButtons/BtnCommentSender.swift
@@ -1,14 +1,15 @@
 //
-//  BtnBase.swift
+//  BtnCommentSender.swift
 //  Reggio Audace
 //
-//  Created by Michele on 24/01/19.
+//  Created by Michele on 25/01/19.
 //  Copyright © 2019 LEN Solution. All rights reserved.
 //
 
 import UIKit
 
-class BtnBase: UIButton
+class BtnCommentSender: BtnBase
 {
+  
 
 }
---
+++ Reggio Audace/Buttons/RoundShapedButtons/BtnRadio.swift
@@ -0,0 +1,22 @@
+//
+//  BtnRadio.swift
+//  Reggio Audace
+//
+//  Created by Michele on 24/01/19.
+//  Copyright © 2019 LEN Solution. All rights reserved.
+//
+
+import UIKit
+
+class BtnRadio: BtnBase
+{
+   override var BorderWidth: CGFloat
+    {
+      return 2.0
+    }
+    override var BorderColor: CGColor
+        {
+            return GARNETCOLOR.cgColor
+        }
+   
+}
