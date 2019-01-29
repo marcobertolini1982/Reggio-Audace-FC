af4ec254468fb94ea583fa3390578b2dbfcf8a81
Michele <marco.bertolini@lemsolution.it>
2019-01-29 17:28:32 +0100

    ccc

--- Reggio Audace/ViewControllers/CollectionControllers/VcrPosts.swift
+++ Reggio Audace/ViewControllers/CollectionControllers/VcrPosts.swift
@@ -107,7 +107,11 @@ class VcrPosts: VcrBase,ProPostsObs
         l_Cell.lbl_Comments.text = "\(self.POSTS[l_Index].num_postmessages ?? 0)"
         l_Cell.lbl_Reactions.text = "\(self.POSTS[l_Index].num_reactions ?? 0)"
         l_Cell.BtnCommentsEvent = self.OnBtnCommentsclick
-        l_Cell.img_prg_file.image = nil
+        if l_Cell.img_prg_file.image != nil
+        {
+            l_Cell.img_prg_file.image = nil
+        }
+        l_Cell.SetUiImageFile(prg_file: self.POSTS[l_Index].prg_file)
         
         // Return
         return l_Cell
